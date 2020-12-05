2d0697cb2a452517ce156c88a6ce8bd6dcdcf94b
han <70646757+han-sae@users.noreply.github.com>
2020-12-05 11:22:01 +0000

    updated image, changed dimensions

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'hanw'
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/3x.gif') {
+      myImage.setAttribute('src','images/3x2.gif');
+    } else {
+      myImage.setAttribute('src','images/3x.gif');
+    }
+}
